c35956260fd155169d7567a0b4ffd79f37404034
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-03-24 20:27:55 -0300

    ajustando o card person

--- src/components/layout/CardCharacter.tsx
+++ src/components/layout/CardCharacter.tsx
@@ -2,8 +2,8 @@ import style from './Cards.module.css'
 
 function CardCharacter({src, text }: { src:string, text: string}) {
         return (
-            <div className={style.content}>
-        <section className={`${style.card_home1}, ${style.card_person}`}>
+        <div className={style.content}>
+        <section className={`${style.card_home1} ${style.card_person}`}>
             <img src={src}  />
             <p>{text}</p>
         </section>
--- src/components/pages/CardPerson.tsx
+++ src/components/pages/CardPerson.tsx
@@ -2,7 +2,14 @@ import CardCharacter from "../layout/CardCharacter"
 import justNatsuki from '../../assets/justNatsuki.png'
 
 function CardPerson() {
-    return <CardCharacter src={justNatsuki} text='Natsuki'/>
+    return (
+        <>
+            <CardCharacter src={justNatsuki} text='Natsuki'/>
+            <CardCharacter src={justNatsuki} text='Natsuki'/>
+            <CardCharacter src={justNatsuki} text='Natsuki'/>
+            <CardCharacter src={justNatsuki} text='Natsuki'/>
+        </>
+    )
 }
 
 export default CardPerson
